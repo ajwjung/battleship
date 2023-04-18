9fef9477335a4a3a1fd1d2ae31f767657942db99
Amy Jung <ajwjung@outlook.com>
2023-04-18 16:10:00 -0700

    Set up HTML webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -6,6 +7,12 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Battleship",
+      template: "./src/index.html",
+    }),
+  ],
   module: {
     rules: [
       {
