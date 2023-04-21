f7f2e9a4b56af2c1137523c304c7343c3a9c86f3
Amy Jung <ajwjung@outlook.com>
2023-04-21 13:02:18 -0700

    Update placeShip test to cover length of ship

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,12 +1,16 @@
 const Ship = require("../modules/ship");
 const Gameboard = require("../modules/gameboard");
 
-test("Carrier placed at A5 (top of ship)", () => {
+test("Carrier placed vertically at A5 to A9", () => {
     const carrier = Ship(5);
     const testBoard = Gameboard();
     testBoard.placeShip(carrier, [5, "A"]);
 
     expect(testBoard.board[4][0].ship).toBe(carrier);
+    expect(testBoard.board[5][0].ship).toBe(carrier);
+    expect(testBoard.board[6][0].ship).toBe(carrier);
+    expect(testBoard.board[7][0].ship).toBe(carrier);
+    expect(testBoard.board[8][0].ship).toBe(carrier);
 });
 
 test("Gameboard receives attack and carrier is hit", () => {
