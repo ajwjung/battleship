a21dfafd9585a94b19c29877c182fb1a20288645
Amy Jung <ajwjung@outlook.com>
2023-04-24 17:23:14 -0700

    Update tests with describe

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,55 +1,57 @@
 const Ship = require("../modules/ship");
 const Gameboard = require("../modules/gameboard");
 
-test("Carrier placed vertically at A5 to A9", () => {
-    const carrier = Ship(5);
-    const testBoard = Gameboard();
-    testBoard.placeShip(carrier, [5, "A"]);
-
-    expect(testBoard.board[4][0].ship).toBe(carrier);
-    expect(testBoard.board[5][0].ship).toBe(carrier);
-    expect(testBoard.board[6][0].ship).toBe(carrier);
-    expect(testBoard.board[7][0].ship).toBe(carrier);
-    expect(testBoard.board[8][0].ship).toBe(carrier);
-});
-
-test("Gameboard receives attack and carrier is hit", () => {
-    const carrier = Ship(5);
-    const testBoard = Gameboard();
-    testBoard.placeShip(carrier, [5, "A"]);
-    testBoard.receiveAttack([5, "A"]);
+describe("Gameboard", () => {
+    test("Carrier placed vertically at A5 to A9", () => {
+        const carrier = Ship(5);
+        const testBoard = Gameboard();
+        testBoard.placeShip(carrier, [5, "A"]);
     
-    expect(carrier.numHits).toEqual(1);
-});
-
-test("Gameboard receives attack but attack misses", () => {
-    const carrier = Ship(5);
-    const testBoard = Gameboard();
-    testBoard.placeShip(carrier, [5, "A"]);
-    testBoard.receiveAttack([3, "E"]);
+        expect(testBoard.board[4][0].ship).toBe(carrier);
+        expect(testBoard.board[5][0].ship).toBe(carrier);
+        expect(testBoard.board[6][0].ship).toBe(carrier);
+        expect(testBoard.board[7][0].ship).toBe(carrier);
+        expect(testBoard.board[8][0].ship).toBe(carrier);
+    });
     
-    expect(testBoard.missedAttacks[0]).toEqual([3, "E"]);
-});
-
-test("Gameboard reports all ships have sunk", () => {
-    const allShips = [];
-    const destroyer = Ship(3);
-    const patrol = Ship(2);
-    allShips.push(destroyer);
-    allShips.push(patrol);
-
-    const testBoard = Gameboard();
-    testBoard.placeShip(destroyer, [5, "E"]);
-    testBoard.placeShip(patrol, [1, "J"]);
-
-    // Attack destroyer
-    testBoard.receiveAttack([5, "E"]);
-    testBoard.receiveAttack([6, "E"]);
-    testBoard.receiveAttack([7, "E"]);
+    test("Gameboard receives attack and carrier is hit", () => {
+        const carrier = Ship(5);
+        const testBoard = Gameboard();
+        testBoard.placeShip(carrier, [5, "A"]);
+        testBoard.receiveAttack([5, "A"]);
+        
+        expect(carrier.numHits).toEqual(1);
+    });
     
-    // Attack patrol boat
-    testBoard.receiveAttack([1, "J"]);
-    testBoard.receiveAttack([2, "J"]);
-
-    expect(testBoard.allShipsSunk(allShips)).toBe(true);
-})
+    test("Gameboard receives attack but attack misses", () => {
+        const carrier = Ship(5);
+        const testBoard = Gameboard();
+        testBoard.placeShip(carrier, [5, "A"]);
+        testBoard.receiveAttack([3, "E"]);
+        
+        expect(testBoard.missedAttacks[0]).toEqual([3, "E"]);
+    });
+    
+    test("Gameboard reports all ships have sunk", () => {
+        const allShips = [];
+        const destroyer = Ship(3);
+        const patrol = Ship(2);
+        allShips.push(destroyer);
+        allShips.push(patrol);
+    
+        const testBoard = Gameboard();
+        testBoard.placeShip(destroyer, [5, "E"]);
+        testBoard.placeShip(patrol, [1, "J"]);
+    
+        // Attack destroyer
+        testBoard.receiveAttack([5, "E"]);
+        testBoard.receiveAttack([6, "E"]);
+        testBoard.receiveAttack([7, "E"]);
+        
+        // Attack patrol boat
+        testBoard.receiveAttack([1, "J"]);
+        testBoard.receiveAttack([2, "J"]);
+    
+        expect(testBoard.allShipsSunk(allShips)).toBe(true);
+    });
+});
--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -2,32 +2,34 @@ const Ship = require("../modules/ship");
 const Gameboard = require("../modules/gameboard");
 const Player = require("../modules/player");
 
-test("Players can take turns", () => {
-    const player = Player("Bob");
-    const computer = Player("AI");
-    
-    player.startTurn();
-    expect(player.checkTurn()).toBe(true);
-    expect(computer.checkTurn()).toBe(false);
+describe("Player", () => {
+    test("Players can take turns", () => {
+        const player = Player("Bob");
+        const computer = Player("AI");
+        
+        player.startTurn();
+        expect(player.checkTurn()).toBe(true);
+        expect(computer.checkTurn()).toBe(false);
 
-    player.endTurn(computer);
-    expect(player.checkTurn()).toBe(false);
-    expect(computer.checkTurn()).toBe(true);
+        player.endTurn(computer);
+        expect(player.checkTurn()).toBe(false);
+        expect(computer.checkTurn()).toBe(true);
+    });
+
+    test("Player can attack computer (opponent)", () => {
+        const player = Player("Bob");
+        const computer = Player("AI");
+        const destroyer = Ship(3);
+        const computerBoard = Gameboard();
+        computerBoard.placeShip(destroyer, [4, "B"]);
+        player.startTurn();
+        player.makeAttack(computer, computerBoard, [4, "B"]);
+
+        expect(computerBoard.board[3][1].ship.numHits).toEqual(1);
+    });
 });
 
-test("Player can attack computer (opponent)", () => {
-    const player = Player("Bob");
-    const computer = Player("AI");
-    const destroyer = Ship(3);
-    const computerBoard = Gameboard();
-    computerBoard.placeShip(destroyer, [4, "B"]);
-    player.startTurn();
-    player.makeAttack(computer, computerBoard, [4, "B"]);
-
-    expect(computerBoard.board[3][1].ship.numHits).toEqual(1);
-});
-
-describe("Computer Player", () => {
+describe("Computer", () => {
     const player = Player("Bob");
     const computer = Player("AI");
     const playerBoard = Gameboard();
