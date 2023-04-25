bdc0f024e4902556230be11372d869d3a0de37a0
Amy Jung <ajwjung@outlook.com>
2023-04-24 17:18:11 -0700

    Check array contains coordinates

---
+++ src/modules/checkArraysContain.js
@@ -0,0 +1,10 @@
+function arrayContainsCoordinates(arr, coordinates) {
+    for (let i = 0; i < arr.length; i += 1) {
+        if (coordinates[0] === arr[i][0]
+            && coordinates[1] === arr[i][1]) return true;
+    };
+
+    return false;
+};
+
+module.exports = arrayContainsCoordinates;
