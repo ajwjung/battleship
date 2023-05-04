ff1bae6e7a5e3ed928dc2e6f9d672110260b990d
Amy Jung <ajwjung@outlook.com>
2023-05-04 14:56:05 -0700

    Generate coordinates for different scenarios

--- src/modules/generateCoordinates.js
+++ src/modules/generateCoordinates.js
@@ -1,20 +1,41 @@
-import arrayContainsCoordinates from "./checkArraysContains.js";
+const arrayContainsCoordinates = require("./checkArraysContains");
 
 const GenerateCoordinates = (() => {
-    function getRandomCoordinates(successfulHits, missedAttacks) {
-        const lettersKey = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+    const lettersKey = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
     
+    function getRandomCoordinates() {
+        const row = Math.floor(Math.random() * 10) + 1;
+        const col = lettersKey[Math.floor(Math.random() * lettersKey.length)];
+
+        return [row, col];
+    };
+
+    function getRandomPlacement(takenSpots, ship) {
+        const row = Math.floor(Math.random() * 10) + 1;
+        const colIndex = Math.floor(Math.random() * lettersKey.length)
+    
+        if (row + ship.length > 10 || colIndex + ship.length > 10
+            || arrayContainsCoordinates(takenSpots, [row, lettersKey[colIndex]])) {
+            return getRandomPlacement(takenSpots, ship);
+        } 
+    
+        return [row, lettersKey[colIndex]];
+    };
+
+    function getUnusedCoordinates(successfulHits, missedAttacks) {
         const row = Math.floor(Math.random() * 10) + 1;
         const col = lettersKey[Math.floor(Math.random() * lettersKey.length)];
     
-        if (arrayContainsCoordinates(successfulHits, [row, col]) || arrayContainsCoordinates(missedAttacks, [row, col])) {
-            return getRandomCoordinates(successfulHits, missedAttacks);
+        if (arrayContainsCoordinates(successfulHits, [row, col])
+        || arrayContainsCoordinates(missedAttacks, [row, col])) {
+            return getUnusedCoordinates(successfulHits, missedAttacks);
         }
     
         return [row, col];
     };
+    
 
-    return { getRandomCoordinates };
+    return { getRandomCoordinates, getRandomPlacement, getUnusedCoordinates };
 })();
 
-export default GenerateCoordinates;
+module.exports = GenerateCoordinates;
