5dfdd89b1dd505f396c1096d35032432ff453ecb
Amy Jung <ajwjung@outlook.com>
2023-05-04 15:23:59 -0700

    fix: Broken code for placing cpu ships

--- src/modules/gameComponents.js
+++ src/modules/gameComponents.js
@@ -10,26 +10,20 @@ const Game = (() => {
         const takenSquares = [];
         
         allShips.forEach(ship => {
-            const [x, y] = GenerateCoordinates.getRandomPlacement(
-                takenSquares, ship
-            ); // [1, "A"] format
+            const [x, y] = GenerateCoordinates.getRandomPlacement(takenSquares, ship); // [1, "A"] format
             const randomBoolean = Math.random() < 0.5;
             const isHorizontal = randomBoolean;
 
             for (let i = 0; i < ship.length; i += 1) {
                 if (isHorizontal) {
-                    computerGameboard.placeShip([x + i, y]);
-                    takenSquares.push([x + i, y])
-                    const square = document.body.querySelector(
-                        `#opponent-board #${y}${x + i}`
-                    );
+                    computerGameboard.placeShip(ship, [x + i, y]);
+                    takenSquares.push([x + i, y]);
+                    const square = document.body.querySelector(`#opponent-board #${y}${x + i}`);
                     square.classList.add("highlight");
                 } else {
-                    computerGameboard.placeShip([x, getLetter(y, i)]);
+                    computerGameboard.placeShip(ship, [x, getLetter(y, i)]);
                     takenSquares.push([x, getLetter(y, i)]);
-                    const square = document.body.querySelector(
-                        `#opponent-board #${getLetter(y, i)}${x}`
-                    );
+                    const square = document.body.querySelector(`#opponent-board #${getLetter(y, i)}${x}`);
                     square.classList.add("highlight");
                 }
             };
@@ -37,6 +31,10 @@ const Game = (() => {
 
         return takenSquares;
     };
+
+    // function cpuTurnToAttack(computer, player, playerBoard, playerShips) {
+
+    // };
 
     return { placeComputerShips };
 })();
