42a1044605870d5e8147b55aee87148ebdc4e83c
Amy Jung <ajwjung@outlook.com>
2023-05-04 15:04:07 -0700

    Change conversion fn to return indices

--- src/modules/convertCoordinates.js
+++ src/modules/convertCoordinates.js
@@ -2,10 +2,11 @@ const lettersKey = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
 
 const Coordinates = (() => {
     function convertCoordinates(coordinates) {
+        // Converts [2, "D"] to [1, 3]
         const [row, col] = coordinates;
         const colIndex = lettersKey.indexOf(col);
         
-        return [row, colIndex];
+        return [row - 1, colIndex];
     };
 
     return { convertCoordinates };
