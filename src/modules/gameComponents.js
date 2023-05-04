e4ea00dc825f159ec791cf1bac0239a7c3ec771c
Amy Jung <ajwjung@outlook.com>
2023-05-04 15:12:07 -0700

    fix: Cpu ships displayed but not placed on board

--- src/modules/gameComponents.js
+++ src/modules/gameComponents.js
@@ -6,27 +6,36 @@ const Game = (() => {
         return lettersKey[lettersKey.indexOf(letter) + i];
     };
 
-    function placeComputerShips(allShips) {
+    function placeComputerShips(computerGameboard, allShips) {
         const takenSquares = [];
         
         allShips.forEach(ship => {
-            const [x, y] = GenerateCoordinates.getRandomPlacement(takenSquares, ship); // [1, "A"] format
+            const [x, y] = GenerateCoordinates.getRandomPlacement(
+                takenSquares, ship
+            ); // [1, "A"] format
             const randomBoolean = Math.random() < 0.5;
             const isHorizontal = randomBoolean;
 
-
             for (let i = 0; i < ship.length; i += 1) {
                 if (isHorizontal) {
+                    computerGameboard.placeShip([x + i, y]);
                     takenSquares.push([x + i, y])
-                    const square = document.body.querySelector(`#opponent-board #${y}${x + i}`)
+                    const square = document.body.querySelector(
+                        `#opponent-board #${y}${x + i}`
+                    );
                     square.classList.add("highlight");
                 } else {
+                    computerGameboard.placeShip([x, getLetter(y, i)]);
                     takenSquares.push([x, getLetter(y, i)]);
-                    const square = document.body.querySelector(`#opponent-board #${getLetter(y, i)}${x}`);
+                    const square = document.body.querySelector(
+                        `#opponent-board #${getLetter(y, i)}${x}`
+                    );
                     square.classList.add("highlight");
                 }
             };
         });
+
+        return takenSquares;
     };
 
     return { placeComputerShips };
